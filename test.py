793eaaf39b27dc475e0c0c5e6137769835e46afe
Alexey <alexey.zhukovich@gmail.com>
2023-11-26 22:33:28 +0300

    add all test construction

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import unittest
 import json
 from nonlinear_equations import Nonlinear_equations
 
+
 class Test_newton(unittest.TestCase):
     '''
     Тесты для метода ньютона
@@ -24,6 +25,7 @@ class Test_newton(unittest.TestCase):
     def tests_exceptionst(self):
         pass
 
+
 class Test_simplify_newton(unittest.TestCase):
     '''
     Тесты для упрощенного метода ньютона
@@ -36,6 +38,99 @@ class Test_simplify_newton(unittest.TestCase):
         pass
 
     def test3(self):
+        pass
+
+
+class Secant_method(unittest.TestCase):
+    '''
+    Тесты для метода секущих
+    '''
+
+    def test_correct(self):
+        pass
+
+    def tests_incorrect(self):
+        pass
+
+    def tests_exceptionst(self):
+        pass
+
+
+class Method_half(unittest.TestCase):
+    '''
+    Тесты для метода половинного деления
+    '''
+
+    def test_correct(self):
+        pass
+
+    def tests_incorrect(self):
+        pass
+
+    def tests_exceptionst(self):
+        pass
+
+
+class Method_chord(unittest.TestCase):
+    '''
+    Тесты для метода хорд
+    '''
+
+    def test_correct(self):
+        pass
+
+    def tests_incorrect(self):
+        pass
+
+    def tests_exceptionst(self):
+        pass
+
+
+
+class Theorem_edges_root(unittest.TestCase):
+    '''
+    Тесты для теоремы о границах расположения корней алгебраического уравнения
+    '''
+
+    def test_correct(self):
+        pass
+
+    def tests_incorrect(self):
+        pass
+
+    def tests_exceptionst(self):
+        pass
+
+
+
+class Theorem_descartes(unittest.TestCase):
+    '''
+    Тесты для Теоремы Декарта о количестве действительных корней
+    '''
+
+    def test_correct(self):
+        pass
+
+    def tests_incorrect(self):
+        pass
+
+    def tests_exceptionst(self):
+        pass
+
+
+
+class Theorem_gua(unittest.TestCase):
+    '''
+    Тесты для теоремы Гюа о необходимом условии действительности всех корней алгебраического уравнения
+    '''
+
+    def test_correct(self):
+        pass
+
+    def tests_incorrect(self):
+        pass
+
+    def tests_exceptionst(self):
         pass
 
 
